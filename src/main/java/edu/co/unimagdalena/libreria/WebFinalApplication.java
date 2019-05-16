9782776a576d73f9b1658e8455713172fd192f6b
Koutawars <admikouta@gmail.com>
2019-05-16 12:06:51 -0500

    init

---
+++ src/main/java/edu/co/unimagdalena/libreria/WebFinalApplication.java
@@ -0,0 +1,13 @@
+package edu.co.unimagdalena.libreria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebFinalApplication.class, args);
+	}
+
+}
